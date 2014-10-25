11cece3d387e7b7ec9dae1b744b84c3de22dcaf5
Radoslav RadoRado Georgiev <radorado@hackbulgaria.com>
2014-10-25 16:34:10 +0300

    Linting 3rd example

--- How.Camp2014/3-Sharing-State/state.js
+++ How.Camp2014/3-Sharing-State/state.js
@@ -1,34 +1,66 @@
-var cluster = require("cluster"),
-      os = require("os"),
-      http = require("http"),
-      fs = require("fs"),
-      PORT = process.env.PORT || 8000,
-      server,
-      state = 0;
+/*global require, process, console, __filename*/
 
-function clusterIt() {
-  if(cluster.isMaster) {
-    os.cpus().forEach(function() {
-      cluster.fork();
+(function() {
+  'use strict';
+  var cluster = require('cluster'),
+        os = require('os'),
+        http = require('http'),
+        fs = require('fs'),
+        PORT = process.env.PORT || 8000,
+        server,
+        state = 0;
+
+
+  function saveState(worker, state) {
+    var currentState,
+          file = worker + '.json';
+
+    try {
+      currentState = require('./' + file);
+    } catch(e) {
+      currentState = {};
+    }
+
+    console.log(currentState);
+    currentState[worker] = state;
+
+    fs.writeFile(file, JSON.stringify(currentState), function(err, res) {
+      if(err) {
+        throw err;
+      }
+
+      console.log('State saved for ' + worker);
+      console.log(res);
     });
-  } else {
-    workWork();
   }
-}
 
 
-function workWork() {
-  server = http.createServer(function(req, res) {
-    state += 1;
-    var message = ["I am", cluster.worker.id, "And my sate is", state].join(" ");
-    console.log(message);
-    fs.readFile(__filename, "utf8", function(err, data) {
-      res.end(data);
+  function clusterIt() {
+    if(cluster.isMaster) {
+      os.cpus().forEach(function() {
+        cluster.fork();
+      });
+    } else {
+      workWork();
+    }
+  }
+
+
+  function workWork() {
+    server = http.createServer(function(req, res) {
+      state += 1;
+      saveState(cluster.worker.id, state);
+      var message = ['I am', cluster.worker.id, 'And my state is', state].join(' ');
+      console.log(message);
+      fs.readFile(__filename, 'utf8', function(err, data) {
+        res.end(data);
+      });
     });
-  });
 
-  console.log("Listening at port " + PORT);
-  server.listen(PORT);
-}
+    console.log('Listening at port ' + PORT);
+    server.listen(PORT);
+  }
 
-clusterIt();
+  clusterIt();
+} ());
+
